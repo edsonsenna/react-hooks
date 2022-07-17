b7b0ab7316025b70f6a576b11ff1eac3b9120485
Edson Senna - ESJ System <edsonsjr14@outlook.com>
2022-07-17 18:18:04 -0300

    Exercise 02 - Extra 04

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,17 +1,25 @@
-const useLocalStorage = () => {
+const useLocalStorage = ({
+  deserialize = JSON.parse,
+  serialize = JSON.stringify,
+} = {}) => {
+  const removeFromLS = (key = '') => {
+    if (window && window.localStorage && key.length > 0)
+      window.localStorage.removeItem(key);
+  };
   const readFromLS = (key = '') => {
     if (window && window.localStorage && key.length > 0)
-      return JSON.parse(window.localStorage.getItem(key));
+      return deserialize(window.localStorage.getItem(key));
     return '';
   };
 
   const storeOnLS = (key = '', value = '') => {
     if (window && window.localStorage && key.length > 0 && value)
-      window.localStorage.setItem(key, JSON.stringify(value));
+      window.localStorage.setItem(key, serialize(value));
   };
 
   return {
     readFromLS,
+    removeFromLS,
     storeOnLS,
   };
 };
--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import useLocalStorage from './useLocalStorage';
 
-const useLocalStorageState = (key, defaultValue = '') => {
-  const {readFromLS, storeOnLS} = useLocalStorage();
+const useLocalStorageState = (
+  key,
+  defaultValue = '',
+  {deserialize = JSON.parse, serialize = JSON.stringify} = {},
+) => {
+  const {readFromLS, removeFromLS, storeOnLS} = useLocalStorage({
+    deserialize,
+    serialize,
+  });
+
+  const prevKeyRef = useRef(key);
 
   const [state, setState] = React.useState(
     () => readFromLS(key) ?? defaultValue,
   );
 
   React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      removeFromLS(prevKey);
+    }
+    prevKeyRef.current = key;
     storeOnLS(key, state);
-  }, [key, state, storeOnLS]);
+  }, [key, removeFromLS, state, storeOnLS]);
 
   return [state, setState];
 };
