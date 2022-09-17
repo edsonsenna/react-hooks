3e0664843ce6cc07ee9c18abd1eba638d747494a
Edson Senna - ESJ System <edsonsjr14@outlook.com>
2022-09-17 16:26:39 -0300

    Exercise 06 - Extra Credit 03

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,10 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react';
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
+
 import {
   fetchPokemon,
   PokemonDataView,
@@ -21,61 +18,48 @@ const Statuses = {
 };
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  const [pokemon, setPokemon] = React.useState(null);
   const [error, setError] = React.useState(null);
-  const [status, setStatus] = React.useState(Statuses.idle);
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
+  const [computedState, setComputedState] = React.useState({
+    status: Statuses.idle,
+    pokemon: null,
+  });
 
   React.useEffect(() => {
     const fetchPokemonData = async () => {
       if (!pokemonName) return;
 
+      setComputedState({status: Statuses.pending, pokemon: null});
       setError(null);
-      setPokemon(null);
-      setStatus(Statuses.pending);
 
       return fetchPokemon(pokemonName).then(
         pokemonData => {
-          setPokemon(pokemonData);
-          setStatus(Statuses.resolved);
+          setComputedState({status: Statuses.resolved, pokemon: pokemonData});
         },
         error => {
           setError(error);
-          setStatus(Statuses.rejected);
+          setComputedState(oldComputedState => ({
+            ...oldComputedState,
+            status: Statuses.rejected,
+          }));
         },
       );
     };
     fetchPokemonData();
   }, [pokemonName]);
 
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
-  // (This is to enable the loading state when switching between different pokemon.)
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
-
-  if (status === Statuses.idle) {
+  if (computedState.status === Statuses.idle) {
     return 'Submit a pokemon';
-  } else if (status === Statuses.pending) {
+  } else if (computedState.status === Statuses.pending) {
     return <PokemonInfoFallback name={pokemonName} />;
-  } else if (status === Statuses.rejected) {
+  } else if (computedState.status === Statuses.rejected) {
     return (
       <div role="alert">
         There was an error:{' '}
         <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
       </div>
     );
-  } else if (status === Statuses.resolved) {
-    return <PokemonDataView pokemon={pokemon} />;
+  } else if (computedState.status === Statuses.resolved) {
+    return <PokemonDataView pokemon={computedState.pokemon} />;
   }
 
   throw new Error('This should be impossible.');
