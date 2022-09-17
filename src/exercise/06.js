46c4a3211544d65ad8942b38ed5381d3aad81fc4
Edson Senna - ESJ System <edsonsjr14@outlook.com>
2022-09-17 16:43:18 -0300

    Exercise 06 - Extra Credit 04

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -17,6 +17,8 @@ const Statuses = {
   rejected: 'rejected',
 };
 
+const WRONG_POKEMON_ERROR = 'wrong-pokemon-name';
+
 function PokemonInfo({pokemonName}) {
   const [computedState, setComputedState] = React.useState({
     status: Statuses.idle,
@@ -56,12 +58,7 @@ function PokemonInfo({pokemonName}) {
   } else if (status === Statuses.pending) {
     return <PokemonInfoFallback name={pokemonName} />;
   } else if (status === Statuses.rejected) {
-    return (
-      <div role="alert">
-        There was an error:{' '}
-        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
-      </div>
-    );
+    throw new Error(error.message);
   } else if (status === Statuses.resolved) {
     return <PokemonDataView pokemon={pokemon} />;
   }
@@ -81,10 +78,50 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   );
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {hasError: false, errorMessage: ''};
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log('Caught error: ', error.message);
+
+    // Update state so the next render will show the fallback UI.
+    return {hasError: true, errorMessage: error.message || ''};
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log({error, errorInfo});
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+
+      if (this.state.errorMessage.length > 0) {
+        return (
+          <div role="alert">
+            There was an error:{' '}
+            <pre style={{whiteSpace: 'normal'}}>{this.state.errorMessage}</pre>
+          </div>
+        );
+      }
+
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
 }
 
 export default App;
